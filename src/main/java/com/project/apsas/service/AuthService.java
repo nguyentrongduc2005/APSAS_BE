19a6243e3f5aba153eb5d4dcb75f0e1fdf67f088
dvidnguyen <qndjxz@gmail.com>
2025-11-06 11:46:04 +0700

    Implement method generate token and encoder password and build scopes #1

--- src/main/java/com/project/apsas/service/AuthService.java
+++ src/main/java/com/project/apsas/service/AuthService.java
@@ -2,6 +2,7 @@ package com.project.apsas.service;
 
 import com.project.apsas.dto.request.LoginRequest;
 import com.project.apsas.dto.response.LoginResponse;
+import com.project.apsas.entity.User;
 
 public interface AuthService {
     public LoginResponse login(LoginRequest loginRequest);
--- src/main/java/com/project/apsas/service/impl/AuthServiceImpl.java
+++ src/main/java/com/project/apsas/service/impl/AuthServiceImpl.java
@@ -1,5 +1,8 @@
 package com.project.apsas.service.impl;
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
 import com.project.apsas.dto.request.LoginRequest;
 import com.project.apsas.dto.response.LoginResponse;
 import com.project.apsas.entity.User;
@@ -10,9 +13,19 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.StringJoiner;
+
+@Slf4j
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @AllArgsConstructor
@@ -20,6 +33,46 @@ public class AuthServiceImpl implements AuthService {
 
     UserRepository userRepository;
     UserMapper userMapper;
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    protected String SIGNER_KEY;
+
+    private String generateToken(User user) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(user.getName())
+                .issuer("devteria.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .claim("scope", buildScope(user))
+                .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Cannot create token", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String buildScope(User user){
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (!CollectionUtils.isEmpty(user.getRoles()))
+            user.getRoles().forEach(stringJoiner::add);
+
+        return stringJoiner.toString();
+    }
+
+
+
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
