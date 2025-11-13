3ab4a8b86a59f19debdb30c0525226d7bca4511d
Duc <nguyentrongduc447@gmail.com>
2025-11-13 15:45:52 +0700

    test

--- src/main/java/com/project/apsas/integration/jubge/JubgeApiClient.java
+++ src/main/java/com/project/apsas/integration/jubge/JubgeApiClient.java
@@ -1,4 +1,24 @@
 package com.project.apsas.integration.jubge;
 
-public class JubgeApiClient {
+import com.project.apsas.integration.brevo.BrevoFeignConfig;
+import com.project.apsas.integration.jubge.dto.SubmissionRCERequest;
+import com.project.apsas.integration.jubge.dto.SubmissionRCEResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "jubge-api",
+        url = "${jubge.base-url.base-url}"
+)
+public interface JubgeApiClient {
+    @PostMapping("/submissions")
+    SubmissionRCEResponse createAndRunSubmission(
+            @RequestBody SubmissionRCERequest submissionRequest,
+            @RequestParam("base64_encoded") boolean base64Encoded,
+            @RequestParam("wait") boolean wait,
+            @RequestParam("fields") String fields
+    );
+
 }
---
+++ src/main/java/com/project/apsas/integration/jubge/dto/SubmissionRCERequest.java
@@ -0,0 +1,22 @@
+package com.project.apsas.integration.jubge.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SubmissionRCERequest {
+    @JsonProperty("source_code")
+    private String sourceCode;
+
+    @JsonProperty("language_id")
+    private int languageId;
+
+    @JsonProperty("stdin")
+    private String stdin;
+}
