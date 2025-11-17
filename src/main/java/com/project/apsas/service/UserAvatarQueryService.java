4fe234a783ef9c68ee96bb2c9a894ef8e9d0a373
ThanhDat-speed <datvt1943@ut.edu.vn>
2025-11-17 13:10:04 +0700

    update api upload

---
+++ src/main/java/com/project/apsas/service/UserAvatarQueryService.java
@@ -0,0 +1,13 @@
+package com.project.apsas.service;
+
+import com.project.apsas.dto.response.SmallAvatarResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface UserAvatarQueryService {
+
+    SmallAvatarResponse getMySmallAvatar();
+
+    SmallAvatarResponse uploadMyAvatar(MultipartFile file) throws IOException;
+}
