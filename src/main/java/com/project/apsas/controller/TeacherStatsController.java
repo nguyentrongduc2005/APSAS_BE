aaba447206149ec652f90aba0f6f19e07754da53
dvidnguyen <qndjxz@gmail.com>
2025-11-18 11:02:01 +0700

    config authorize for lecturer get total student #34

--- src/main/java/com/project/apsas/controller/TeacherStatsController.java
+++ src/main/java/com/project/apsas/controller/TeacherStatsController.java
@@ -4,6 +4,7 @@ import com.project.apsas.dto.ApiResponse;
 import com.project.apsas.dto.teacher.TeacherStatsDTO;
 import com.project.apsas.service.TeacherStatsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TeacherStatsController {
     private final TeacherStatsService teacherStatsService;
+    @PreAuthorize("hasRole('LECTURER')")
     @GetMapping("/stats/total-students")
     public ApiResponse<TeacherStatsDTO> getTotalStudents() {
         TeacherStatsDTO stats = teacherStatsService.getTeacherStats();
