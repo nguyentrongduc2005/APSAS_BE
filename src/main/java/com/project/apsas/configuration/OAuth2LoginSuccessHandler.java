dcaa73a4e4a6a3682c9d887cdb3fa73a5e2633e3
dvidnguyen <qndjxz@gmail.com>
2025-11-27 00:42:59 +0700

    Oauth for google

--- src/main/java/com/project/apsas/configuration/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/project/apsas/configuration/OAuth2LoginSuccessHandler.java
@@ -14,6 +14,7 @@ import com.project.apsas.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,7 +37,7 @@ import java.util.*;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-
+@Transactional
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final UserRepository userRepository;
